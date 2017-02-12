b77dce5059c49eb2e069f45dfd689066328e47d8
Brent Long <blong123@gmail.com>
2017-02-11 21:04:07 -0800

    init project with ar cam and point cloud depth detection

---
+++ Assets/controllerScript.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class KittyUIController : MonoBehaviour
+{
+	public GameObject m_kitten;
+	private TangoPointCloud m_pointCloud;
+
+	void Start()
+	{
+		m_pointCloud = FindObjectOfType<TangoPointCloud>();
+	}
+
+	void Update ()
+	{
+		
+	}
+
+
+}
